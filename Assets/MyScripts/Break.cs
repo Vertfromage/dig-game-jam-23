0e0b1f030a6741c245c3f8547a1882c4dd149bed
Crystal Parker <vertfromage@gmail.com>
2023-11-11 19:01:59 -0400

    broke the jar

---
+++ Assets/MyScripts/Break.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+    public Sprite BrokenSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "TeaBoy")
+        {
+            GetComponent<SpriteRenderer>().sprite = BrokenSprite;
+        }
+    }
+}
